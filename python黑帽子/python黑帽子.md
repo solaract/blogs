1d11d42c6c1e821c33baeccfa6098146e9248177
solaract <email@example.com>
2023-11-23 17:18:48 +0800

    netcat start

--- python黑帽子/python黑帽子.md
+++ python黑帽子/python黑帽子.md
@@ -69,4 +69,79 @@
    client.close()
    ```
 3. TCP多线程服务端
-   
+   ```py {.line-numbers}
+   import socket
+   import threading
+
+   IP = '0.0.0.0'
+   PORT = 9998
+
+   def main():
+      server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      #设置监听地址
+      server.bind((IP, PORT))
+      #最大连接数设置为5
+      server.listen(5)
+      print(f'[*] Listening on {IP}:{PORT}')
+
+      while True:
+         #保存客户端socket对象及地址
+         client, address = server.accept()
+         print(f'[*] Accepted connection from {address[0]}:{address[1]}')
+         #创建新线程
+         client_handler = threading.Thread(target=handle_client, args=(client,))
+         client_handler.start()
+
+   def handle_client(client_socket):
+      with client_socket as sock:
+         request = sock.recv(1024)
+         print(f'[*] Received:{request.decode("utf-8")}')
+         sock.send(b'ACK')
+
+   if __name__ == '__main__':
+      main()
+   ```
+4. netcat
+   ```py {.line-numbers}
+   import argparse
+   import socket
+   import shlex
+   import subprocess
+   import sys
+   import textwrap
+   import threading
+
+   def execute(cmd):
+      #去除字符串两侧空白字符串
+      cmd = cmd.strip()
+      if not cmd:
+         return
+      #shlex.split()解析命令字符串参数列表
+      #subprocess模块生成子进程
+      #subprocess.check_output运行命令并获取标准输出
+      output = subprocess.check_output(shlex.split(cmd),stderr=subprocess.STDOUT)
+      return output.decode
+   ```
+   - shlex 模块是 Python 中用于解析 shell 命令字符串的模块。它提供了一个 shlex 类，该类允许你将命令字符串解析为适用于 subprocess 模块等的参数列表。
+      ```py {.line-numbers}
+      import shlex
+
+      # 解析简单的命令字符串
+      command_string = "echo 'hello'"
+      command_list = shlex.split(command_string)
+      print(command_list)
+      #['echo', 'hello']
+      ```
+   - subprocess.check_output 是 subprocess 模块提供的一个函数，用于运行命令并获取其标准输出。与 subprocess.run 不同，check_output 函数会直接返回命令的标准输出，而不是一个包含各种信息的 CompletedProcess 对象。
+      ```py {.line-numbers}
+      ##subprocess
+      import subprocess
+
+      try:
+         # 运行命令并获取标准输出
+         output = subprocess.check_output(["cd", "123"], shell=True, text=True)
+         print(output)
+      except subprocess.CalledProcessError as e:
+         print(f"命令执行失败：{e}")
+
+      ```
--- python黑帽子/socket_test.py
+++ python黑帽子/socket_test.py
@@ -27,35 +27,76 @@
 # print(data.decode())
 # client.close()
 
-## TCP多线程客户端
-import socket
-import threading
+# ## TCP多线程服务器
+# import socket
+# import threading
 
-IP = '0.0.0.0'
-PORT = 9998
+# IP = '0.0.0.0'
+# PORT = 9998
 
-def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #设置监听地址
-    server.bind(IP, PORT)
-    #最大连接数设置为5
-    server.listen(5)
-    print(f'[*] Listening on {IP}:{PORT}')
+# def main():
+#     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     #设置监听地址
+#     server.bind((IP, PORT))
+#     #最大连接数设置为5
+#     server.listen(5)
+#     print(f'[*] Listening on {IP}:{PORT}')
 
-    while True:
-        client, address = server.accept()
-        print(f'[*] Accepted connection from {address[0]}:{address[1]}')
-        client_handler = threading.Thread(target=handle_client, args=(client,))
-        client_handler.start()
+#     while True:
+#         #保存客户端socket对象及地址
+#         client, address = server.accept()
+#         print(f'[*] Accepted connection from {address[0]}:{address[1]}')
+#         #创建新线程
+#         client_handler = threading.Thread(target=handle_client, args=(client,))
+#         client_handler.start()
 
-def handle_client(client_socket):
-    with client_socket as sock:
-        request = sock.recv(1024)
-        print(f'[*] Received:{request.decode("utf-8")}')
-        sock.send(b'ACK')
+# def handle_client(client_socket):
+#     with client_socket as sock:
+#         request = sock.recv(1024)
+#         print(f'[*] Received:{request.decode("utf-8")}')
+#         sock.send(b'ACK')
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
+
+# ##netcat
+# import argparse
+# import socket
+# import shlex
+# import subprocess
+# import sys
+# import textwrap
+# import threading
+
+# def execute(cmd):
+#     #去除字符串两侧的空白字符
+#     cmd = cmd.strip()
+#     if not cmd:
+#         return
+      #shlex.split()解析命令字符串参数列表
+      #subprocess模块生成子进程
+      #subprocess.check_output运行命令并获取标准输出
+#     output = subprocess.check_output(shlex.split(cmd),stderr=subprocess.STDOUT)
+#     return output.decode
+
+# ##shelex
+# import shlex
+
+# # 解析简单的命令字符串
+# command_string = "echo 'hello'"
+# command_list = shlex.split(command_string)
+# print(command_list)
+
+##subprocess
+import subprocess
+
+try:
+    # 运行命令并获取标准输出
+    output = subprocess.check_output(["cd", "123"], shell=True, text=True)
+    print(output)
+except subprocess.CalledProcessError as e:
+    print(f"命令执行失败：{e}")
+
 
 
 
